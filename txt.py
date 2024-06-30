7752101145c1448d40019fdcfd4883be51da2cce
Artem <temaevt@gmail.com>
2024-07-01 01:24:22 +0300

    123

--- txt.py
+++ txt.py
@@ -1,3 +1,80 @@
-a = 10
-print(a)
-dsadasdsdadas
+class Cow006Game:
+    def __init__(self):
+        self.players = []
+        self.draw_pile = []
+        self.discard_pile = []
+        self.current_player = None
+
+    def setup_game(self):
+        # Раздать карты игрокам, заполнить колоду
+        pass
+
+    def switch_turn(self):
+        # Сменить ход игрока
+        pass
+
+    def draw_card(self, player):
+        # Взять карту из колоды или из открытой кучи
+        pass
+
+    def play_card(self, player, cards):
+        # Выложить комбинацию карт на стол
+        pass
+
+    def check_winner(self):
+        # Проверить победителя
+        pass
+
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        # Добавить карту в руку
+        pass
+
+    def remove_card(self, card):
+        # Удалить карту из руки
+        pass
+
+import unittest
+
+class TestCow006Game(unittest.TestCase):
+    def test_setup_game(self):
+        # Тест на начало игры
+        pass
+
+    def test_draw_card(self):
+        # Тест на взятие карты
+        pass
+
+    def test_play_card(self):
+        # Тест на выкладывание комбинации карт
+        pass
+
+    def test_check_winner(self):
+        # Тест на проверку победителя
+        pass
+
+class TestCard(unittest.TestCase):
+    def test_card_init(self):
+        # Тест на создание карты
+        pass
+
+class TestHand(unittest.TestCase):
+    def test_add_card(self):
+        # Тест на добавление карты в руку
+        pass
+
+    def test_remove_card(self):
+        # Тест на удаление карты из руки
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
