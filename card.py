98bfc31caebe9274670ce025e4fc9ec72932a6af
Artem <temaevt@gmail.com>
2024-07-04 00:44:37 +0300

    card

--- card.py
+++ card.py
@@ -1,3 +1,13 @@
 class Card:
     def __init__(self, value):
         self.value = value
+        if self.value == 55:
+            self.point = 7
+        elif self.value % 11 == 0:
+            self.point = 5
+        elif self.value % 10 == 0:
+            self.point = 3
+        elif self.value % 5 == 0:
+            self.point = 2
+        else:
+            self.point = 1
